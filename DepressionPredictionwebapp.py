717a3e3a9fdd210141d7e3b682f2b2d7c63741ba
Pranayak Uniyal <65599419+upranayak@users.noreply.github.com>
2023-04-28 21:28:54 +0530

    Update DepressionPredictionwebapp.py

--- DepressionPredictionwebapp.py
+++ DepressionPredictionwebapp.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('F:/Projects/Diabetes Prediction/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 
 # creating a function for Prediction
